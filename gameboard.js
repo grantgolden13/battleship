cfdc0769dbf03c208414b0272e69ead61ef87ea8
grantgolden13 <grantgolden13@gmail.com>
2022-09-06 14:00:47 -0700

    make second test work for gameboard

--- gameboard.js
+++ gameboard.js
@@ -5,27 +5,27 @@ function Gameboard() {
 
     gameboard.board = [
         [0, 0], [1, 0], [2, 0], [3, 0],
-        [1, 0], [1, 1], [1, 2], [1, 3],
-        [2, 0], [2, 1], [2, 2], [2, 3],
-        [3, 0], [3, 1], [3, 2], [3, 3]
+        [0, 1], [1, 1], [2, 1], [3, 1],
+        [0, 2], [1, 2], [2, 2], [3, 2],
+        [0, 3], [1, 3], [2, 3], [3, 3]
     ];
 
-    gameboard.placeShip = function(coords) {
-        let location;
+    gameboard.placeShip = function(coords, length) {
+        let startLocation;
         for (let item of gameboard.board) {
             if (item[0] == coords[0]) {
                 if (item[1] == coords[1]) {
-                    location = gameboard.board.indexOf(item);
-                    gameboard.board[location] = "X";
+                    startLocation = gameboard.board.indexOf(item);
+                    gameboard.board[startLocation] = "X";
+                    for (let i = (length - 1); i > 0; i--) {
+                        gameboard.board[startLocation + i] = "X";
+                    }
                     return gameboard.board;
                 } 
             }
         }
         return gameboard.board;
     }
-    
+
     return gameboard;
 }
-
-const testBoard = Gameboard().placeShip([1, 1]);
-console.log(testBoard);
--- gameboard.test.js
+++ gameboard.test.js
@@ -3,10 +3,21 @@ import Gameboard from './gameboard'
 test('board works', () => {
     const testBoard = Object.create(Gameboard());
     
-    expect(testBoard.placeShip([2, 2])).toEqual([
+    expect(testBoard.placeShip([2, 2], 1)).toEqual([
         [0, 0], [1, 0], [2, 0], [3, 0],
-        [1, 0], [1, 1], [1, 2], [1, 3],
-        [2, 0], [2, 1], "X", [2, 3],
-        [3, 0], [3, 1], [3, 2], [3, 3]
-    ])
-})
+        [0, 1], [1, 1], [2, 1], [3, 1],
+        [0, 2], [1, 2], "X", [3, 2],
+        [0, 3], [1, 3], [2, 3], [3, 3]
+    ]);
+});
+
+test('board works with an actual-length ship', () => {
+    const testBoard = Object.create(Gameboard());
+
+    expect(testBoard.placeShip([0, 2], 4)).toEqual([
+        [0, 0], [1, 0], [2, 0], [3, 0],
+        [0, 1], [1, 1], [2, 1], [3, 1],
+        "X", "X", "X", "X",
+        [0, 3], [1, 3], [2, 3], [3, 3]
+    ]);
+});
