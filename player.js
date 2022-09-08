c3934bd500ed101b0bf32e4c684e529de292dda7
grantgolden13 <grantgolden13@gmail.com>
2022-09-08 12:46:21 -0700

    add a comment as reminder

--- player.js
+++ player.js
@@ -10,7 +10,8 @@ function Player() {
     player.randomAttack = function(enemyGameboard) {
         const randomIndex = Math.floor(Math.random() * 50);
         const randomCoords = enemyGameboard.board[randomIndex];
-        const result = enemyGameboard.receiveAttack(randomCoords)
+        const result = enemyGameboard.receiveAttack(randomCoords);
+        // will need to fix this bc it will fuck up the indexes of the remaining coords
         enemyGameboard.board.splice(randomIndex, 1);
         player.attackedCoords.push(randomCoords);
         return result;
